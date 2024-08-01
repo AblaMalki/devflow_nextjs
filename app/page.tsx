e7b86d6fdee43ab682a0cdc9646bf3f4a60c4f2a
Abla Malki <ablamalki21@gmail.com>
2024-08-01 17:59:25 +0100

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">nextjs + tailwindcss + typescript</h1>
-      <h2 className="h2-bold">nextjs + tailwindcss + typescript</h2>
-      <h3 className="h3-bold">nextjs + tailwindcss + typescript</h3>
-    </div>
-  );
-}
