53ba0dd3d53fbd2d4c49b49a06a50e0146b46508
Abla Malki <ablamalki21@gmail.com>
2024-09-11 20:08:28 +0100

    ask question workflow and Mongo DB creaion and connection

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -7,53 +7,15 @@ import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/home/HomeFilters";
 import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
+import { getQuestions } from "@/lib/actions/question.action";
 
-const questions = [
-  {
-    _id: "1",
-    title: "Case-insensitive 'in' operator in Python",
-    tags: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "author1.png",
-      clerkId: "clerk123",
-    },
-    upvotes: ["user1", "user2"],
-    views: 100,
-    answers: [
-      { content: "Use the lower() function" },
-      { content: "You can try regex." },
-    ],
-    createdAt: new Date("2021-09-01T00:00:00.000Z"),
-  },
-  {
-    _id: "2",
-    title: "How to use React Query?",
-    tags: [
-      { _id: "3", name: "react" },
-      { _id: "4", name: "react-query" },
-    ],
-    author: {
-      _id: "2",
-      name: "Jane Doe",
-      picture: "author2.png",
-      clerkId: "clerk456",
-    },
-    upvotes: ["user3", "user4"],
-    views: 100,
-    answers: [
-      { content: "React Query manages server-state easily" },
-      { content: "It has great caching capabilities" },
-    ],
-    createdAt: new Date("2021-09-01T00:00:00.000Z"),
-  },
-];
 
-const Home = () => {
+
+export default async function Home  ()  {
+
+   const result = await getQuestions({});
+   console.log(result.questions)
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -81,8 +43,8 @@ const Home = () => {
       </div>
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
@@ -130,4 +92,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+
