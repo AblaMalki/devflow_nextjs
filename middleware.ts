b3919a33d77d3a2bb19c94d4ed5a97907045a9e6
Abla Malki <ablamalki21@gmail.com>
2024-12-07 16:19:22 +0100

    clerk redirect

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/ask-question(.*)",
-  "/collection(.*)",
-]);
+export default clerkMiddleware();
 
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) (await auth()).redirectToSignIn();
-});
 export const config = {
   matcher: [
-    // // Public routes
-    // "/",
-    // "/api/webhook",
-    // "/question/:id",
-    // "/tags",
-    // "/tags/:id",
-    // "/profile/:id",
-    // "/community",
-    // "/jobs",
-
-    // // Ignored routes
-    // "/api/chatgpt",
-
-    // General matcher: Skipping Next.js internals and all static files
+    // Protected routes
+    "/ask-question(.*)",
+    "/collection(.*)",
+    "/profile/(.*)",
+    // API routes
+    "/api/(.*)",
+    // General matcher: Skipping Next.js internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
   ],
 };
